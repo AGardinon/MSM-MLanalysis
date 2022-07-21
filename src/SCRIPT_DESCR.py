6ed7c943db86eed9c93cafd03eb9cff5c7d390c0
AGardinon <agardin@LAPTOP-G1.localdomain>
2022-07-21 11:39:58 +0100

    splitted the two script for traj and descriptor analysis

--- src/SCRIPT_DESCR.py
+++ src/SCRIPT_DESCR.py
@@ -31,6 +31,36 @@ traj_dict = dict(
 
 )
 
+# DESCR (from JD routine)
+turbo_param_multi_dict = dict(
+    alpha_max = 8,
+    atom_sigma_r = 0.2,
+    atom_sigma_t = 0.2,
+    atom_sigma_r_scaling = 0.1,
+    atom_sigma_t_scaling = 0.1,
+    amplitude_scaling = 1.0,
+    central_weight = 1.0,
+)
+
+turbo_param_dict = dict(
+    type = "soap_turbo",
+    Zs = [3],
+    species_Z = [1,3,6,8,9,15,21,23], #[1,3,6,8,9,15],
+    l_max = 4,
+    n_species = 8,
+    rcut_hard = 4.5,
+    rcut_soft = 3.5,
+    basis = "poly3gauss",
+    scaling_mode = "polynomial",
+    radial_enhancement = 1,
+    multi = turbo_param_multi_dict,
+)
+
+descr_dict = dict(
+    chunkEval = 250,
+    save_file = True,
+)
+
 # -----------------------------------------------------------------
 #
 #   MAIN PIPE
@@ -45,7 +75,18 @@ trajObj = pT.TrajLoader(**traj_dict)
 # - Reading the amounts of frames needed
 t0 = time.time()
 traj_read = trajObj.readTraj()
-traj_COM_read = trajObj.readTrajCOM(saveFile=True)
+t1 = time.time()
+print(f"{np.round(t1-t0, 1)}s")
+
+# --- 2. Descriptor
+
+print(f"\n# --- Computing the descriptor ---\n")
+
+# - Computing the descriptor
+t0 = time.time()
+descrObj = pD.TURBOdescriptor(turbo_param_dict)
+print(f"Paramenters:\n{descrObj.dscr_str}")
+descrObj.ChunkEval(traj_read, frame_tuple=trajObj.readFrames, chunk=descr_dict['chunkEval'])
 t1 = time.time()
 print(f"{np.round(t1-t0, 1)}s")
     
